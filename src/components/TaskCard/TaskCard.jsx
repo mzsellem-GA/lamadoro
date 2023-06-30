be3ca096660efa111922ad97a48b9cae495c5931
NeeluT <nilufar.tavakoli@gmail.com>
2023-06-30 09:46:05 -0400

    Style Tasks and Timer marley sellem, mzsellem@hotmail.com

--- src/components/TaskCard/TaskCard.jsx
+++ src/components/TaskCard/TaskCard.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 import { getToken } from "../../utilities/users-service";
 import * as tasksApi from "../../utilities/tasks-api";
+import "../../pages/TaskListPage/TaskListPage.css";
 
 export default function TaskCard({ task, setTasks, setUpdated, updated }) {
    const token = getToken();
@@ -42,18 +43,38 @@ export default function TaskCard({ task, setTasks, setUpdated, updated }) {
          setError(error.message);
       }
    };
+   const updateChange= () => {
+      document.getElementById('task_text').classList.add('d-none');
+      document.getElementById('update_input').classList.remove('d-none');
+      document.getElementById('update_input').classList.add('d-flex');
+      document.getElementById('task_text').classList.remove('d-flex');
+   }
 
    return (
-      <div>
-         <h1>{task.text}</h1>
-         <input type="text" name="text" onChange={handleChange} />
-
-         <button onClick={() => handleSubmit(task._id)}>Update</button>
-         <button onClick={() => handleDelete(task._id)}>X</button>
-         {error && <p>{error}</p>}
-         <Link to="/detail" state={{ task }}>
-            <button>Add Timer</button>
-         </Link>
+      <div className=" my-1 mx-auto d-flex justify-content-between align-items-center mx-auto">
+         <div className="w-50 d-flex">
+            <h5 className="d-flex box-center mx-2 text-white" id="task_text">{task.text}</h5>
+            <div className="d-none" id="update_input" >
+               <input type="text" name="text" onChange={handleChange} value={task.text}/>
+               <button onClick={() => handleSubmit(task._id)}>Update</button>
+            </div>
+            
+         </div>
+         <div className="w-50 d-flex align-items-center justify-content-end">
+            <Link to="/detail" state={{ task }}>
+               <button className="rounded-3 py-1 px-3 border-0">Add Timer</button>
+            </Link>
+            <button className="d-flex box-center p-1 rounded-circle update_btn border-0" onClick={() => updateChange()}>
+               <span className="d-flex box-center m-2">-</span>
+            </button>
+            <button className="delete_btn d-flex box-center p-1 rounded-circle border-0" onClick={() => handleDelete(task._id)}>
+               <span className="d-flex box-center m-2">X</span>
+            </button>
+            {error && <p>{error}</p>}
+            
+         </div>
+         
+         
       </div>
    );
 }
--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -55,20 +55,37 @@ export default function Timer({ quotes }) {
    }, [timer]);
 
    return (
-      <div className="App">
-         <h1>{state.task.text}</h1>
-         <div>{secondsLeft}</div>
-         {randomQuote && (
+      <div className="w-100 h-100 d-flex justify-content-center align-items-center App">
+         <div className="tasks-title col-10">
+            <div className="my-4 task-top text-white justify-content-start d-flex w-100">
+               <h1 className="mx-4">{state.task.text}</h1>
+            </div>
+            {randomQuote && (
             <div>
-               <h2>Random Quote:</h2>
+               <h2 className="text-white">Random Quote:</h2>
                <p>
                   {randomQuote.text} - {randomQuote.byName}
                </p>
             </div>
          )}
-         <button onClick={start}>Start</button>
-         <button onClick={reset}>Reset</button>
-         <button onClick={pause}>Pause</button>
+            <div className="tasks-container rounded-3 w-100 d-flex justify-content-center align-itmes-center h-100">
+               <div className="col-8 p-2">
+                  <div>
+                     <img src="lamadoro-logo.png" alt="image" className="w-100 h-100"/>
+                  </div>
+                  <div className="d-flex w-100 box-center">
+                     <div className="rounded-3 py-2 w-25 timer_part">{secondsLeft}</div>
+                  </div>
+                  <div className="d-flex w-100 box-center my-2">
+                     <button className="mx-3 rounded-3 border-0 px-4" onClick={start}>Start</button>
+                     <button className="mx-3 rounded-3 border-0 px-4" onClick={reset}>Reset</button>
+                     <button className="mx-3 rounded-3 border-0 px-4" onClick={pause}>Pause</button>
+                  </div>
+                  
+               </div>
+               
+            </div>
+         </div>
       </div>
    );
 }
